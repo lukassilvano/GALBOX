26a2f819d5fa4b6fab1035c8f2bbf9586251b3ac
Lukas <lukassilvano@gmail.com>
2020-11-16 20:57:30 -0300

    Atualização HomePage
    Falta logo, vídeo e polir.

---
+++ js/script_home.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#button-mais").click(function(){
+		window.location.href = "pages/produtos.html";
+		
+	});
+	$("#carrinho").click(function(){
+		window.location.href = "pages/carrinho.html";
+		
+	});
+});
