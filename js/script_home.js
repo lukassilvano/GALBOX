4b8852d2564b1bfca10c5c34c7b322b8661a452b
Lukas <lukassilvano@gmail.com>
2020-11-18 20:26:28 -0300

    Google Maps adicionado.

--- js/script_home.js
+++ js/script_home.js
@@ -7,4 +7,21 @@ $(document).ready(function() {
 		window.location.href = "pages/carrinho.html";
 		
 	});
+	initMap();
 });
+
+      // Initialize and add the map
+      function initMap() {
+        // The location of Uluru
+        const GALBOX = { lat: -25.453582, lng: -49.252981 };
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 16,
+          center: GALBOX,
+        });
+        // The marker, positioned at Uluru
+        const marker = new google.maps.Marker({
+          position: GALBOX,
+          map: map,
+        });
+      }
